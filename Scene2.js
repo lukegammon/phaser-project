a062b7915320143d70406186a02f7eb1eeec19d3
Luke Gammon <lukegammon@gmx.com>
2021-02-07 22:50:01 +0000

    update push

--- Scene2.js
+++ Scene2.js
@@ -7,11 +7,47 @@ class Scene2 extends Phaser.Scene {
     this.background = this.add.tileSprite(0,0, config.width, config.height, "background");
     this.background.setOrigin(0,0);
 
-    this.ship1 = this.add.image(config.width / 2 - 50, config.height/2, "ship");
-    this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
-    this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
+    this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, "ship");
+    this.ship2 = this.add.sprite(config.width /2, config.height/ 2, "ship2");
+    this.ship3 = this.add.sprite(config.width /2 + 50, config.height / 2, "ship3");
+
+    this.player = this.physics.add.sprite(config.width / 2 -8, config.height - 64, "player");
+    this.player.play("thrust");
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.player.setCollideWorldBounds(true);
+
+    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    this.powerUps = this.physics.add.group();
+
+    var maxObjects = 4;
+    for(var i = 0; i <= maxObjects; i++) {
+      var powerUp = this.physics.add.sprite(16, 16, "power-up");
+      this.powerUps.add(powerUp);
+      powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+      if(Math.random() > 0.5) {
+        powerUp.play("red");
+      } else {
+        powerUp.play("grey");
+      }
+      powerUp.setVelocity(100, 100);
+      powerUp.setCollideWorldBounds(true);
+      powerUp.setBounce(1);
+    }
+
+    this.input.on('gameobjectdown', this.destroyShip, this);
+
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
 
     this.add.text(20, 20, "Playing game", {font:"25px Arial", fill:"yellow"});
+
+
   };
 
   moveShip(ship, speed) {
@@ -27,14 +63,36 @@ class Scene2 extends Phaser.Scene {
     ship.x = randomX;
   }
 
+  destroyShip(pointer, gameObject) {
+    gameObject.setTexture("explosion");
+    gameObject.play("explode");
+  }
+
+  movePlayerManager() {
+    if(this.cursorKeys.left.isDown) {
+      this.player.setVelocityX(-gameSettings.playerSpeed);
+    } else if (this.cursorKeys.right.isDown) {
+      this.player.setVelocityX(gameSettings.playerSpeed);
+    }
+
+    if(this.cursorKeys.up.isDown) {
+      this.player.setVelocityY(-gameSettings.playerSpeed);
+    }else if(this.cursorKeys.down.isDown){
+      this.player.setVelocityY(gameSettings.playerSpeed);
+    }
+  }
+
   update() {
     this.moveShip(this.ship1, 1);
     this.moveShip(this.ship2, 2);
     this.moveShip(this.ship3, 3);
 
     this.background.tilePositionY -= 0.5;
+
+    this.movePlayerManager();
+    if(Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+      this.shootBeam();
+    }
   }
-
-
 
 }
--- game.js
+++ game.js
@@ -1,9 +1,21 @@
+var gameSettings = {
+  playerSpeed: 120
+}
+
 var config = {
   width: 256,
   height: 272,
   backgroundColor: 0x000000,
   scene: [Scene1, Scene2],
+  physics:{
+    default: "arcade",
+    arcade:{
+      debug: false
+    }
+  }
 };
+
+
 
 
 var game = new Phaser.Game(config);
